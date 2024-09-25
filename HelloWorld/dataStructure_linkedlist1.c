ddfbd67b96c9c866cffe514ebcc50cf8d4e3b41d
lena <adellakwon@gmail.com>
2024-09-25 16:36:37 +1000

    data structure - linked list

--- HelloWorld/dataStructure_linkedlist1.c
+++ HelloWorld/dataStructure_linkedlist1.c
@@ -16,22 +16,42 @@
 
 // 단순 연결 리스트의 노드 구조 정의===============================================
 // 노드의 구조 : 데이터필드 + 포인터필드
-typedef struct ListNode { 
-	int data; // 데이터필드 부분 정수형 변수 사용해 공간 할당
-	struct ListNode* link; // 다음 노드 주소값 저장, ListNode포인터 사용, 노드 전체를 가리키는 포인트 변수
-}listNode;
+typedef struct Node { 
+
+	int data; // 데이터필드 부분 정수형 변수
+
+	// 다음 노드는 지금 현재 노드와 같은 노드??가 되어야 해서
+	// 다음노드의 포인터 변수는 struct Node 타입의 포인터가 되어야 함.
+	// 다음 노드(struct Node타입의 구조체) 주소값 저장, Node포인터 사용, 노드 전체를 가리키는 포인트 변수
+	struct Node* link; //struct Node타입의 구조체의 주소를 저장하는 포인터 변수
+}Node; // 재정의 하지 않으면 매번 struct Node라고 해줘야 함.
+
+
+// 주소를 저장하는 변수(포인터변수 : 8 바이트)
+// 포인터는 64비트 시스템에서 무조건 8바이트를 차지함, 32비트에서 4바이트
+// (같은 시스템에서 모든 자료형 int,double,char,Node...의 포인터 크기는 같음)
+// 8바이트짜리 포인터가 생긴것
+// 첫 노드의 주소를 저장하는 변수 (포인터변수: 8바이트)
+// Node* head = NULL; 
+// NULL: 포인터가 가리키는 대상이 없는 경우
+
 
 
 // 리스트의 헤드 노드 구조 정의=====================================================
 // 데이터 필드 필요 없으므로 리스트 첫번째 원소 가리키기 위한 링크 필드만 생성
 typedef struct { 
-	listNode* head; // 자료형 ListNode를 가리킬 수 있도록 정의
+	// 연결리스트에서 가장 중요한 시작노드 주소 저장하는 헤드 필요
+	// 포인터변수 head 선언
+	// 여기서 주의할 점은 head 선언하면 노드가 생성되는게 아니라
+	// 8 바이트 짜리 포인터 변수가 선언된다는 점.
+	Node* head; // 자료형 Node를 가리킬 수 있도록 정의
+
 }linkedList_h;
 
 
 // 연결리스트 생성====================================================================
 linkedList_h* createLinkedList_h(void) { 
-	linkedList_h* H; // 헤드노드 생성
+	linkedList_h* H; // 헤드노드 선언
 
 	// 헤드 사이즈 만큼 동적할당 후 리턴된 void를 linkdeList_h* 로 캐스팅
 	// 메모리할당받은 후 해당 주소를 반환
@@ -45,17 +65,17 @@ linkedList_h* createLinkedList_h(void) {
 // 리스트 마지막 노드 뒤에 삽입 연산하며, x값은 100,200,300로 가정함
 void addNode(linkedList_h* H, int x) { 
 	
-	// 구조체 listNode사용해 새노드, 마지막 노드 선언
-	listNode* NewNode; // 새 노드 
-	listNode* LastNode; // 연결리스트 노드 많은 경우 마지막 노드 찾기 위한 변수
+	// 구조체 Node사용해 새노드, 마지막 노드 선언
+	Node* NewNode; // 새 노드 
+	Node* LastNode; // 연결리스트 노드 많은 경우 마지막 노드 찾기 위한 변수
 
 	// malloc함수 이용해 NewNode 동적할당, 할당받은 메모리공간 주소값을 NewNode에 저장
 	// 즉 새롭게 할당된 메모리 공간은 NewNode가 가리킴
-	NewNode = (listNode*)malloc(sizeof(listNode)); // 노드 동적할당
+	NewNode = (Node*)malloc(sizeof(Node)); // 노드 동적할당
 
-	// 구조체listNode를 이용해 생성한 NewNode의 멤버변수 data, link에 값 할당
-	NewNode -> data = x; // 데이터 값 매개변수로 받음.
-	NewNode -> link = NULL; // 주소값 현재 NULL
+	// 구조체Node를 이용해 생성한 NewNode의 멤버변수 data, link에 값 할당
+	NewNode->data = x; // 데이터 값 매개변수로 받음.
+	NewNode->link = NULL; // 주소값 현재 NULL
 
 	// 현재 열결된 노드가 없기 때문에 조건만족해서 실행됨
 	// 첫번째 노드 생성(100)된 후 두번째 호출시에는 거짓이므로 실행되지 않음
@@ -90,8 +110,8 @@ void addNode(linkedList_h* H, int x) {
 // 마지막 노드 삭제 후 그 선행노드의 링크필드값은 NULL이 되어야 함
 // 삭제시 연결노드 한 개인지 여러 개인지 고려해야 함
 void deleteNode(linkedList_h* H) { 
-	listNode* prevNode; // 삭제되는 노드의 선행노드 선언, 자료형 ListNode를 가리킴
-	listNode* delNode; // 삭제되는 노드 선언
+	Node* prevNode; // 삭제되는 노드의 선행노드 선언, 자료형 ListNode를 가리킴
+	Node* delNode; // 삭제되는 노드 선언
 
 	if (H->head == NULL)return; // 공백 리스트인 경우, 삭제연산 중단
 	
@@ -103,8 +123,8 @@ void deleteNode(linkedList_h* H) {
 		return;
 	}
 	else { // 리스트 노드가 여러개인 경우 : 마지막까지 찾아가야 함
-		prevNode = H -> head; // 마지막 노드 앞 노드를 가리키도록 위치지정, 우선은 헤드가 가리키는 곳
-		delNode = H -> head -> link; // 삭제할 노드 처음 위치는 prevNode의 다음 위치로 지정
+		prevNode = H->head; // 마지막 노드 앞 노드를 가리키도록 위치지정, 우선은 헤드가 가리키는 곳
+		delNode = H->head->link; // 삭제할 노드 처음 위치는 prevNode의 다음 위치로 지정
 
 		// delNode가 가리키는 주소값이 NULL이 아니므로 참이므로 실행됨
 		// 연결 리스트 마지막 원소가 아니라는 것
@@ -142,16 +162,21 @@ void deleteNode(linkedList_h* H) {
 // 포인터를 매개변수로 받아 처리하는 연산 코드 만들기
 void deleteitNode(linkedList_h* H, int deldata) {
 	
-	listNode* delNode;
-	listNode* prevNode;
+	Node* delNode; 
+	Node* prevNode;   
 	prevNode = H->head;
 
 	while (prevNode->link->data != deldata) {
 		prevNode = prevNode->link;
 	}
-	
+	//============================XXXXXXXXXXXXXXXXXXXXXX====================
+	// 교재 124 설명과 다름
+	// 삭제전 prevNode 링크 필드값을 delNode 후행노드 링크값으로 먼저 수정해야
+	// prevNode가 삭제노드 후행노드의 주소를 잃어버리지 않을 수 있다고 했는데,
+	// 여기는 삭제노드를 먼저 바꿈.
 	delNode = prevNode->link;
 	prevNode->link = delNode->link;
+	//==============================XXXXXXXXXXXXXXXXXXXXX===================
 	free(delNode);
 
 	printf("%d 데이터 값을 가진 노드가 삭제되었습니다. \n", deldata);
@@ -189,15 +214,15 @@ void deleteitNode(linkedList_h* H, int deldata) {
 void additNode(linkedList_h* H, int itdata, int adddata) {
 
 	//itdata: prevNode의 데이터, adddata : 삽입할 노드의 데이터
-	listNode* prevNode;
+	Node* prevNode;
 	prevNode = H-> head;
 
 	while (prevNode->data != itdata) {
 		prevNode = prevNode -> link;
 	}
 
-	listNode* NewNode;
-	NewNode = (listNode*)malloc(sizeof(listNode));
+	Node* NewNode;
+	NewNode = (Node*)malloc(sizeof(Node));
 	NewNode->data = itdata;
 	NewNode->link = NULL;
 
@@ -216,7 +241,7 @@ void additNode(linkedList_h* H, int itdata, int adddata) {
 // 여기에선 반드시 있는 것으로 가정.
 void serchNode(linkedList_h* H, int itdata) {
 	
-	listNode* tempNode;
+	Node* tempNode;
 	tempNode = H->head;
 
 	while (tempNode->data != itdata) {
@@ -229,33 +254,34 @@ void serchNode(linkedList_h* H, int itdata) {
 //교재코드
 // 연결 리스트에서 특정 노드를 검색하여 삭제하고자 하는 연산
 // 연결리스트의 헤드노드(H)와 찾고자 하는 데이터값(itdata)을 매개변수로 전달받음 
-void findandDeleteNode(linkedList_h* H, int itdata) {
-	listNode* prevNode; // 삭제될 노드 선행노드 선언
-	listNode* delNode; // 삭제될 노드 선언
-
-	// 헤드노드 값이 NULL인 경우면 공백리스트이므로 검색중단
-	if (H->head == NULL)return; // 공백리스트
-	
-	
-	prevNode = H; // 노드 앞을 가리키도록 우선 위치를 헤드로 정함
-	delNode = H->head; // 삭제노드가 연결리스트 첫번째 가리키도록 함
-
-	// delNode가 NULL이 아닌 경우는 헤드노드에 노드가 연결되어 있는 경우- 참
-	while (delNode != NULL) {
-		// 매개변수로 받은 idata값과 delNode데이터값 비교
-		// 참인 경우 삭제데이터 찾음
-		if (delNode->data == itdata) {
-			//특정노드 삭제하는 deleteitNode함수 호출, 연산
-			deleteitNode(H, prevNode, delNode);
-			return;
-		}
-		// 같지 않을 경우 delNode는 다음 노드를 가리킴
-		else {
-			prevNode = delNode; // prevNode는 delNode의 위치로 이동
-			delNode = delNode->link; //delNode는 저장된 링크값 따라가서 그 다음 노드를 가리킴
-		}
-	}
-}
+//void findandDeleteNode(linkedList_h* H, int itdata) {
+//	listNode* prevNode; // 삭제될 노드 선행노드 선언
+//	listNode* delNode; // 삭제될 노드 선언
+//
+//	// 헤드노드 값이 NULL인 경우면 공백리스트이므로 검색중단
+//	if (H->head == NULL)return; // 공백리스트
+//	
+//	
+//	prevNode = H; // 노드 앞을 가리키도록 우선 위치를 헤드로 정함
+//	delNode = H->head; // 삭제노드가 연결리스트 첫번째 가리키도록 함
+//
+//	// delNode가 NULL이 아닌 경우는 헤드노드에 노드가 연결되어 있는 경우- 참
+//	while (delNode != NULL) {
+//		// 매개변수로 받은 idata값과 delNode데이터값 비교
+//		// 참인 경우 삭제데이터 찾음
+//		if (delNode->data == itdata) {
+//			//특정노드 삭제하는 deleteitNode함수 호출, 연산
+//			deleteitNode(H, prevNode, delNode);
+//			return;
+//		}
+//		// 같지 않을 경우 delNode는 다음 노드를 가리킴
+//		else {
+//			prevNode = delNode; // prevNode는 delNode의 위치로 이동
+//			delNode = delNode->link; //delNode는 저장된 링크값 따라가서 그 다음 노드를 가리킴
+//			//while문이 반복됨
+//		}
+//	}
+//}
 
 
 
@@ -263,7 +289,7 @@ void findandDeleteNode(linkedList_h* H, int itdata) {
 // 노드 순서대로 리스트를 출력하는 함수=============================================
 void printList(linkedList_h* L) {
 	
-	listNode* p;
+	Node* p;
 	printf("List = (");
 	p = L-> head;
 
